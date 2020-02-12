e17b58200f787074b1f17012ec22907e2ab426d9
pc2005 <philchen2005@gmail.com>
2020-02-12 17:12:45 -0500

    Minor updates.

--- src/pc_test.sh
+++ src/pc_test.sh
@@ -2,7 +2,7 @@
 python demo.py ctdet --demo /mnt/share/valetpark/Rec20190328120637/FrontCamera --load_model ../models/ctdet_coco_dla_2x.pth
 
 ## multi-pose
-# python demo.py multi_pose --demo webcam --load_model ../models/multi_pose_dla_3x.pth
+python demo.py multi_pose --demo webcam --load_model ../models/multi_pose_dla_3x.pth
 
 ## 3d object detection
-# python demo.py ddd --demo /mnt/share/valetpark/Rec20190328120637/FrontCamera --load_model ../models/ddd_3dop.pth
+python demo.py ddd --demo /mnt/DAT_DATA_001/datasets/Kitti/data_semantics/testing/image_2 --load_model ../models/ddd_3dop.pth

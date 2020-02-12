adbcd25d54f83d216336bc1eb2a52756956a6059
pc2005 <philchen2005@gmail.com>
2020-02-12 15:51:38 -0500

    Updated DCNv2 for pytorch 1.x.

---
+++ src/pc_test.sh
@@ -0,0 +1,3 @@
+python demo.py ctdet --demo /mnt/share/valetpark/Rec20190328120637/FrontCamera --load_model ../models/ctdet_coco_dla_2x.pth
+
+# python demo.py multi_pose --demo webcam --load_model ../models/multi_pose_dla_3x.pth

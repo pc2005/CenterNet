638ac5842b3371d47cf13be5a5dbcd4a92dc34e0
pc2005 <philchen2005@gmail.com>
2020-04-23 12:18:57 -0400

    Added comments to training process.

--- src/main.py
+++ src/main.py
@@ -19,14 +19,14 @@ from trains.train_factory import train_factory
 def main(opt):
   # set random seed
   torch.manual_seed(opt.seed)
-  
+
   # set cudnn auto-tuner
   torch.backends.cudnn.benchmark = not opt.not_cuda_benchmark and not opt.test
-  
-  # 
+
+  #
   Dataset = get_dataset(opt.dataset, opt.task)
   opt = opts().update_dataset_info_and_set_heads(opt, Dataset)
-  
+
   # print and log all arguments
   print(opt)
   logger = Logger(opt)
@@ -49,7 +49,7 @@ def main(opt):
 
   ## Load data
   print('Setting up data...')
-  
+
   # validation data
   val_loader = torch.utils.data.DataLoader(
       Dataset(opt, 'val'),
@@ -78,29 +78,40 @@ def main(opt):
   print('Starting training...')
   best = 1e10
   for epoch in range(start_epoch + 1, opt.num_epochs + 1):
+
     mark = epoch if opt.save_all else 'last'
-    log_dict_train, _ = trainer.train(epoch, train_loader)  # epoch training
+
+    # epoch training
+    log_dict_train, _ = trainer.train(epoch, train_loader)
     logger.write('epoch: {} |'.format(epoch))
 
+    # log train stats
     for k, v in log_dict_train.items():
       logger.scalar_summary('train_{}'.format(k), v, epoch)
       logger.write('{} {:8f} | '.format(k, v))
 
+    # model saving logics
     if opt.val_intervals > 0 and epoch % opt.val_intervals == 0:
+      # save checkpoing model
       save_model(os.path.join(opt.save_dir, 'model_{}.pth'.format(mark)),
                  epoch, model, optimizer)
+
+      # run validation
       with torch.no_grad():
         log_dict_val, preds = trainer.val(epoch, val_loader)
 
+      # log validation stats
       for k, v in log_dict_val.items():
         logger.scalar_summary('val_{}'.format(k), v, epoch)
         logger.write('{} {:8f} | '.format(k, v))
 
+      # save best model
       if log_dict_val[opt.metric] < best:
         best = log_dict_val[opt.metric]
         save_model(os.path.join(opt.save_dir, 'model_best.pth'),
                    epoch, model)
     else:
+      # save last model
       save_model(os.path.join(opt.save_dir, 'model_last.pth'),
                  epoch, model, optimizer)
 
